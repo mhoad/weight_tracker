2ca24f7d6d98ed40c3c076e306a27101dce7bae1
Mark Hoad <markdhoad@gmail.com>
2012-03-25 16:02:24 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeightTracker::Application.config.secret_token = '64d49eef0f7cf3df5bc5fc6a6ad7897417416cdbda7e03fbaed09fa870aeb1674528d101c0956064248d8b1a7e303fe07751bf5753658ace879dc5959908e581'
